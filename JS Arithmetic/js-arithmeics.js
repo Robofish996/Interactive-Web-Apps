fa7a68e2de6ffb4a4ea71f3de127f19cfa8dcbbd
Robofish996 <Matthew.A.Riddle@outlook.com>
2023-01-16 12:36:51 +0200

    Completed JS Arithmetic Challenge

---
+++ JS Arithmetic/js-arithmeics.js
@@ -0,0 +1,39 @@
+/* My Calculations*/
+document.write(" ----- Area Of Shapes JS Arithmetic Challenge ----- <br>")
+
+/* Constant Variables:*/
+const pi = 3.1419;
+const half = 0.5;
+
+/* Variables for measurements*/
+let rectangleLength = 10;
+let rectangleBreadth = 17;
+let circleRadius = 25;
+let triangleBase = 20;
+let triangleHeight = 9;
+
+
+/* Rectangle area calculation*/
+   let rectangleAnswer = rectangleLength * rectangleBreadth
+
+/* Append your rectangle answer:*/
+document.write("<br>The area of the given rectangle is : " );
+document.write(rectangleAnswer);
+
+
+/* circle area calculation*/
+
+let circleArea = pi * circleRadius * circleRadius
+
+/* Append your circle answer:*/
+document.write("<br>The area of the given circle is : ");
+document.write(circleArea)
+
+
+
+/* triangle area calculation*/
+   let triangleArea = triangleBase / 2 * triangleHeight
+
+/* Append your triangle answer:*/
+document.write("<br>The area of the given triangle is : " );
+document.write(triangleArea)
